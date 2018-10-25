5bb8ba289b9d151ad31bf34e8bbccc9fab908eaf
Tylor <tylor@ubuntu.ubuntu-domain>
2018-10-25 09:18:30 -0700

    updated String Matrix folder to include and example Opserver class and made some small modbus/sunspec changes

--- Modbus/include/Modbus.h
+++ Modbus/include/Modbus.h
@@ -6,50 +6,6 @@
 
 // Modbus Includes
 #include <modbus/modbus-tcp.h>
-// BOOST Libs
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/xml_parser.hpp>
-#include <boost/foreach.hpp>
-
-// SunSpec Model
-// - This model uses boosts property tree to parse the xml models for
-// - modbus register blocks.
-// - https://www.technical-recipes.com/2014/using-boostproperty_tree/
-class SunSpecModel {
-public:
-    SunSpecModel (unsigned int did, unsigned int offset)
-        : offset_(offset) {
-        // create filename using a base path, then pad the did number and append
-        // to the base path. The sunspec models are provided as xml so that will be
-        // the file type that is appended ot the end of the filename.
-        std::stringstream ss;
-        ss << "./models/smdx/smdx_";
-        ss << std::setfill ('0') << std::setw(5) << did;
-        ss << ".xml";
-        std::string filename = ss.str();
-
-        // Use boosts xml parser to read file and store as member variable.
-        boost::property_tree::xml_parser::read_xml(filename, smdx_);
-        name_ = smdx_.get <std::string> ("sunSpecModels.model.<xmlattr>.name", "");
-        length_ = smdx_.get <unsigned int> ("sunSpecModels.model.<xmlattr>.len", 0);
-        std::cout << "SunSpec Model Found"
-            << "\n\tName: " << name_
-            << "\n\tLength: " << length_ << std::endl;
-    };
-
-    ~SunSpecModel () {};
-
-    std::string GetName () {
-        return name_;
-    };
-
-private:
-    unsigned int offset_;
-    unsigned int length_;
-    std::string name_;
-    boost::property_tree::ptree smdx_;
-    std::map <std::string, std::string> points_;
-};
 
 class Modbus {
 public:
@@ -68,9 +24,7 @@ public:
     void WriteBlock (std::string name);
 
 private:
-    // member variables
-    modbus_t *context_ptr_;
-    std::vector <std::shared_ptr <SunSpecModel>> models_;
+
 };
 
 #endif // MODBUS_H
--- StringMatrixToVectorObjects/main.cpp
+++ StringMatrixToVectorObjects/main.cpp
@@ -4,38 +4,70 @@
 
 #include "tsu.h"
 
-struct ScheduleHeader {
-    int time;
-    std::string control;
-    unsigned int setting;
-    ScheduleHeader (int time, std::string control, unsigned int setting)
-        : time(time), control(control), setting(setting) {
+class Operator {
+public:
+    Operator (const std::string& filename)
+        : index_(0), file_path_(filename) {
+        tsu::string_matrix matrix = tsu::FileToMatrix("data.csv", ',', 3);
+
+        // reserve memory for each row within the matrix
+        schedule_.reserve(matrix.size());
+
+        // loop through each row of the matrix and set each column to it's
+        // header data type
+        for (const auto &col : matrix) {
+            schedule_.emplace_back (stoi(col[0]), col[1], stoul(col[2]));
+        }
     };
+
+    void Loop (const unsigned int time) {
+
+        // loop through each row of schedule looking for current time
+        for (unsigned int i = index_; i < schedule_.size(); i++) {
+            ScheduleHeader& row = schedule_[i];
+
+            if (row.time == time && index_ != i) {
+
+                // if the time is found check control type
+                if (row.control == "import") {
+                    std::cout << "importing at... " << row.setting << std::endl;
+                } else if (row.control == "export") {
+                    std::cout << "exporting at... " << row.setting << std::endl;
+                } else {
+                    std::cout << "idleing" << std::endl;
+                }
+
+                // log index so multiple controls are not sent and to reduce
+                // search time.
+                index_ = i;
+            }
+        }
+    };
+
+private:
+    // since the file columns are known we can create and object to represent
+    // the values within the column.
+    struct ScheduleHeader {
+        int time;
+        std::string control;
+        unsigned int setting;
+
+        ScheduleHeader (int time, std::string control, unsigned int setting)
+            : time(time), control(control), setting(setting) {
+        };
+    };
+
+private:
+    unsigned int index_;
+    std::string file_path_;
+    std::vector <ScheduleHeader> schedule_;
 };
 
 using namespace std;
 
 int main()
 {
-    tsu::string_matrix matrix = tsu::FileToMatrix("data.csv", ',', 3);
-
-    // reserve memory for each row within the matrix
-    vector <ScheduleHeader> schedule;
-    schedule.reserve(matrix.size());
-
-    // loop through each row of the matrix and set each column to it's
-    // header data type
-    for (const auto &row : matrix) {
-        schedule.emplace_back (stoi(row[0]), row[1], stoul(row[2]));
-    }
-
-    for (const auto &entry : schedule) {
-        cout << "Time:\t" << entry.time << "\t" << entry.control << " at "
-             << entry.setting << '\n';
-    }
-
-    string test = "10.0000";
-    float val = stof(test);
-    cout << val << endl;
+    Operator OP("data.csv");
+    OP.Loop(10);
     return 0;
 }
